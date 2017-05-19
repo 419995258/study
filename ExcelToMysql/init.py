f9b0956de0e4846f35ba09ae0c9c325069f3dd4c
PB <419995258@qq.com>
2017-05-19 16:38:47 +0800

    添加把excel保存到mysql

--- ExcelToMysql/init.py
+++ ExcelToMysql/init.py
@@ -3,7 +3,7 @@ import MySQLdb
 import xlrd
 
 
-book = xlrd.open_workbook("f:\\test\\excel.xlsx")
+book = xlrd.open_workbook("f:\\test\\data.xlsx")
 sheet = book.sheet_by_name("Sheet1")
 
 conn= MySQLdb.connect(
@@ -16,27 +16,16 @@ conn= MySQLdb.connect(
 
 cursor = conn.cursor()
 
-sql = "insert into test1(name,age) values(%s,%s)"
-'''cursor.execute(sql,('1','2'))'''
-# wb = lw(filename='f:\\test\\excel.xlsx')
-# ws = wb.get_sheet_by_name(wb.get_sheet_names()[0])  # <worksheet "data">
-#
-# rows = ws.get_highest_row()  # 最大行数
-# columns = ws.get_highest_column()  # 最大列数
-#
-# data = []
-# for rx in range(2, rows + 1):
-#         for cx in range(1, columns + 1):
-#                 data.append(str(ws.cell(row=rx, column=cx).value))
-#                 cursor.execute(sql,
-#                     (data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9]))
-#         data = []
+sql = "insert into test1(Keyword,C1,UT) values(%s,%s,%s)"
+
+
+# 循环获取数据并保存数据
 for r in range(1, sheet.nrows):
-        name = sheet.cell(r,0).value
-        age = sheet.cell(r,1).value
+        Keyword = sheet.cell(r,0).value
+        C1 = sheet.cell(r,1).value
+        UT = sheet.cell(r, 2).value
 
-
-        cursor.execute(sql,(name,age))
+        cursor.execute(sql,(Keyword,C1,UT))
 
 
 
