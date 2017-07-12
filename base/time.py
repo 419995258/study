ee229c9b03074e730dffeec6584e36ac4c6bc231
pb <419995258@qq.com>
2017-07-12 15:29:25 +0800

    更新优化文档目录

--- base/time.py
+++ base/time.py
@@ -1,8 +1,8 @@
 import time
 
-print time.time()
+# print base.time()
 print time.localtime()
-print time.strftime('%Y-%m-%d %H:%M:%S',time.localtime())
+print time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
 for i in range(100):
     # time.sleep(i*0.01)
     print i
--- logPackage/myLog.py
+++ logPackage/myLog.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-import logging
 import getpass
-import sys
-import time
+import logging
+
+from base import time
+
 
 # 定义MyLog类
 class MyLog(object):
@@ -13,7 +14,7 @@ class MyLog(object):
       user = getpass.getuser()
       self.logger = logging.getLogger(user)
       self.logger.setLevel(logging.DEBUG)
-      nowTime = 'Log-' + time.strftime('%Y%m%d', time.localtime(time.time()))#当前时间
+      nowTime = 'Log-' + time.strftime('%Y%m%d', time.localtime(base.time()))#当前时间
       # logFile = './' + sys.argv[0][0:-3] + '.log' #日志文件名-------项目名命名
       logFile = './' + nowTime + '.log'  # 日志文件名-----时间命名
       formatter = logging.Formatter('%(asctime)-12s %(levelname)-8s %(name)-10s %(message)-12s')
