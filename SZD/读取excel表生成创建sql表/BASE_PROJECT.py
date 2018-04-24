ccd0f160215e96192fcab9858ceebbfd6da016e4
pb <419995258@qq.com>
2018-04-24 17:40:45 +0800

    添加base_project表

--- SZD/读取excel表生成创建sql表/BASE_PROJECT.py
+++ SZD/读取excel表生成创建sql表/BASE_PROJECT.py
@@ -7,16 +7,16 @@ sys.setdefaultencoding('utf8')
 book = xlrd.open_workbook("data.xlsx")
 sheet = book.sheet_by_name("a")
 
-sql = "create table BASE_PRO_T_MAP("
+sql = "create table BASE_PROJECT("
 sql = sql + " ID       VARCHAR2(50) not null,"
-commentsql = "comment on table BASE_PRO_T_MAP is '项目教师MAP';\n"
+commentsql = "comment on table BASE_PROJECT is '培训项目表';\n"
 # 循环获取数据并保存数据，获取从第2行到30的数据
 # for r in range(2, sheet.nrows):
-for r in range(2, 30):
+for r in range(33, 65):
         col = sheet.cell(r,1).value
         comment = sheet.cell(r, 2).value
         sql = sql + col + " VARCHAR2(50),"
-        commentsql = commentsql + "comment on column BASE_PRO_T_MAP." + col + " is '" + comment + "';\n"
+        commentsql = commentsql + "comment on column BASE_PROJECT." + col + " is '" + comment + "';\n"
         # print col
         # print comment
 
